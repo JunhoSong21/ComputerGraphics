946cb5a362cb952f47b4c8fd230f5e443da63ee0
JunhoSong21 <164911108+JunhoSong21@users.noreply.github.com>
2024-09-09 04:29:13 +0900

    WarmingUp 1 End

--- WarmingUp/Project1/1. 행렬다루기.cpp
+++ WarmingUp/Project1/1. 행렬다루기.cpp
@@ -9,33 +9,31 @@ int SelectCommand();
 int Amatrix[16];
 int Bmatrix[16];
 
-int main(void) {
-	while (1) {
-		LoadingMakingMatrixs();
-		ShowMatrixs();
+int main() {
+	LoadingMakingMatrixs();
+	ShowMatrixs();
 
-		while (1) {
-			SelectCommand();
-		}
+	while (true) {
+		SelectCommand();
 	}
 }
 
 void LoadingMakingMatrixs() { // 행렬 생성하는 함수
 	std::cout << "새로운 2개의 행렬 생성중..." << std::endl;
 	
-	for (int i = 0; i < 2; ++i) { // 두개의 행렬 랜덤으로 생성하는 코드
+	for (auto i = 0; i < 2; ++i) { // 두개의 행렬 랜덤으로 생성하는 코드
 		int MatrixNum[16];
 		int SelectTWO[2];
 
-		for (int j = 0; j < 16; ++j) {
+		for (auto j = 0; j < 16; ++j) {
 			MatrixNum[j] = rand() % 2;
 		}
 
-		for (int j = 0; j < 2; ++j) {
+		for (auto j = 0; j < 2; ++j) {
 			SelectTWO[j] = rand() % 16;
 		}
 
-		while (1) {
+		while (true) {
 			if (SelectTWO[0] == SelectTWO[1]) {
 				SelectTWO[1] = rand() % 16;
 			}
@@ -44,7 +42,7 @@ void LoadingMakingMatrixs() { // 행렬 생성하는 함수
 			}
 		}
 
-		for (int j = 0; j < 2; ++j) {
+		for (auto j = 0; j < 2; ++j) {
 			MatrixNum[SelectTWO[j]] = 2;
 		}
 
@@ -54,7 +52,7 @@ void LoadingMakingMatrixs() { // 행렬 생성하는 함수
 			}
 		}
 		else if (i == 1) {
-			for (int j = 0; j < 16; ++j) {
+			for (auto j = 0; j < 16; ++j) {
 				Bmatrix[j] = MatrixNum[j];
 			}
 		}
@@ -97,11 +95,141 @@ int SelectCommand() {
 		}
 		std::cout << "행렬의 뺄셈" << std::endl;
 	}
-	else if (CommandOpt == 113) {
-		
+	else if (CommandOpt == 114) { // 행렬식의 값
+		int AmatrixDet =
+			(Amatrix[0] * ((Amatrix[5] * (Amatrix[10] * Amatrix[15] - Amatrix[11] * Amatrix[14]))
+				- (Amatrix[6] * (Amatrix[9] * Amatrix[15] - Amatrix[11] * Amatrix[13]))
+				+ (Amatrix[7] * (Amatrix[9] * Amatrix[14] - Amatrix[10] * Amatrix[13]))))
+			- (Amatrix[4] * (Amatrix[4] * (Amatrix[10] * Amatrix[15] - Amatrix[11] * Amatrix[13]))
+				- (Amatrix[5] * (Amatrix[8] * Amatrix[15] - Amatrix[11] * Amatrix[12]))
+				+ (Amatrix[7] * (Amatrix[8] * Amatrix[13] - Amatrix[9] * Amatrix[12])))
+			+ (Amatrix[2] * (Amatrix[4] * (Amatrix[9] * Amatrix[15] - Amatrix[11] * Amatrix[13]))
+				- (Amatrix[5] * (Amatrix[8] * Amatrix[15] - Amatrix[11] * Amatrix[12]))
+				+ (Amatrix[7] * (Amatrix[8] * Amatrix[13] - Amatrix[9] * Amatrix[12])))
+			- (Amatrix[3] * (Amatrix[4] * (Amatrix[9] * Amatrix[14] - Amatrix[10] * Amatrix[13]))
+				- (Amatrix[5] * (Amatrix[8] * Amatrix[14] - Amatrix[10] * Amatrix[12]))
+				+ (Amatrix[6] * (Amatrix[8] * Amatrix[13] - Amatrix[9] * Amatrix[12])));
+		int BmatrixDet =
+			(Bmatrix[0] * ((Bmatrix[5] * (Bmatrix[10] * Bmatrix[15] - Bmatrix[11] * Bmatrix[14]))
+				- (Bmatrix[6] * (Bmatrix[9] * Bmatrix[15] - Bmatrix[11] * Bmatrix[13]))
+				+ (Bmatrix[7] * (Bmatrix[9] * Bmatrix[14] - Bmatrix[10] * Bmatrix[13]))))
+			- (Bmatrix[4] * (Bmatrix[4] * (Bmatrix[10] * Bmatrix[15] - Bmatrix[11] * Bmatrix[13]))
+				- (Bmatrix[5] * (Bmatrix[8] * Bmatrix[15] - Bmatrix[11] * Bmatrix[12]))
+				+ (Bmatrix[7] * (Bmatrix[8] * Bmatrix[13] - Bmatrix[9] * Bmatrix[12])))
+			+ (Bmatrix[2] * (Bmatrix[4] * (Bmatrix[9] * Bmatrix[15] - Bmatrix[11] * Bmatrix[13]))
+				- (Bmatrix[5] * (Bmatrix[8] * Bmatrix[15] - Bmatrix[11] * Bmatrix[12]))
+				+ (Bmatrix[7] * (Bmatrix[8] * Bmatrix[13] - Bmatrix[9] * Bmatrix[12])))
+			- (Bmatrix[3] * (Bmatrix[4] * (Bmatrix[9] * Bmatrix[14] - Bmatrix[10] * Bmatrix[13]))
+				- (Bmatrix[5] * (Bmatrix[8] * Bmatrix[14] - Bmatrix[10] * Bmatrix[12]))
+				+ (Bmatrix[6] * (Bmatrix[8] * Bmatrix[13] - Bmatrix[9] * Bmatrix[12])));
+
+		std::cout << "행렬식의 값" << std::endl;
+		std::cout << "첫 번째 행렬 : " << AmatrixDet << "\n" << "두 번째 행렬 : " << BmatrixDet << std::endl;
+		return 0;
+	}
+	else if (CommandOpt == 116) { // 전치행렬과 그 행렬식의 값
+		int AmatrixTrans[16], BmatrixTrans[16];
+		for (auto i = 0; i < 16; i += 5) {
+			AmatrixTrans[i] = Amatrix[i];
+			BmatrixTrans[i] = Bmatrix[i];
+		}
+		AmatrixTrans[1] = Amatrix[4];
+		AmatrixTrans[2] = Amatrix[8];
+		AmatrixTrans[3] = Amatrix[12];
+		AmatrixTrans[4] = Amatrix[1];
+		AmatrixTrans[6] = Amatrix[9];
+		AmatrixTrans[7] = Amatrix[13];
+		AmatrixTrans[8] = Amatrix[2];
+		AmatrixTrans[9] = Amatrix[6];
+		AmatrixTrans[11] = Amatrix[14];
+		AmatrixTrans[12] = Amatrix[3];
+		AmatrixTrans[13] = Amatrix[7];
+		AmatrixTrans[14] = Amatrix[11];
+
+		BmatrixTrans[1] = Bmatrix[4];
+		BmatrixTrans[2] = Bmatrix[8];
+		BmatrixTrans[3] = Bmatrix[12];
+		BmatrixTrans[4] = Bmatrix[1];
+		BmatrixTrans[6] = Bmatrix[9];
+		BmatrixTrans[7] = Bmatrix[13];
+		BmatrixTrans[8] = Bmatrix[2];
+		BmatrixTrans[9] = Bmatrix[6];
+		BmatrixTrans[11] = Bmatrix[14];
+		BmatrixTrans[12] = Bmatrix[3];
+		BmatrixTrans[13] = Bmatrix[7];
+		BmatrixTrans[14] = Bmatrix[11];
+
+		std::cout << "전치행렬과 그 행렬식의 값" << std::endl;
+		int i = 0;
+
+		while (i < 13) {
+			std::cout << "|" << AmatrixTrans[i] << " " << AmatrixTrans[i + 1] << " " << AmatrixTrans[i + 2] << " " << AmatrixTrans[i + 3]
+				<< "||" << BmatrixTrans[i] << " " << BmatrixTrans[i + 1] << " " << BmatrixTrans[i + 2] << " " << BmatrixTrans[i + 3] << "|" << std::endl;
+			i += 4;
+		}
+
+		int AmatrixDet =
+			(AmatrixTrans[0] * ((AmatrixTrans[5] * (AmatrixTrans[10] * AmatrixTrans[15] - AmatrixTrans[11] * AmatrixTrans[14]))
+				- (AmatrixTrans[6] * (AmatrixTrans[9] * AmatrixTrans[15] - AmatrixTrans[11] * AmatrixTrans[13]))
+				+ (AmatrixTrans[7] * (AmatrixTrans[9] * AmatrixTrans[14] - AmatrixTrans[10] * AmatrixTrans[13]))))
+			- (AmatrixTrans[4] * (AmatrixTrans[4] * (AmatrixTrans[10] * AmatrixTrans[15] - AmatrixTrans[11] * AmatrixTrans[13]))
+				- (AmatrixTrans[5] * (AmatrixTrans[8] * AmatrixTrans[15] - AmatrixTrans[11] * AmatrixTrans[12]))
+				+ (AmatrixTrans[7] * (AmatrixTrans[8] * AmatrixTrans[13] - AmatrixTrans[9] * AmatrixTrans[12])))
+			+ (AmatrixTrans[2] * (AmatrixTrans[4] * (AmatrixTrans[9] * AmatrixTrans[15] - AmatrixTrans[11] * AmatrixTrans[13]))
+				- (AmatrixTrans[5] * (AmatrixTrans[8] * AmatrixTrans[15] - AmatrixTrans[11] * AmatrixTrans[12]))
+				+ (AmatrixTrans[7] * (AmatrixTrans[8] * AmatrixTrans[13] - AmatrixTrans[9] * AmatrixTrans[12])))
+			- (AmatrixTrans[3] * (AmatrixTrans[4] * (AmatrixTrans[9] * AmatrixTrans[14] - AmatrixTrans[10] * AmatrixTrans[13]))
+				- (AmatrixTrans[5] * (AmatrixTrans[8] * AmatrixTrans[14] - AmatrixTrans[10] * AmatrixTrans[12]))
+				+ (AmatrixTrans[6] * (AmatrixTrans[8] * AmatrixTrans[13] - AmatrixTrans[9] * AmatrixTrans[12])));
+		int BmatrixDet =
+			(BmatrixTrans[0] * ((BmatrixTrans[5] * (BmatrixTrans[10] * BmatrixTrans[15] - BmatrixTrans[11] * BmatrixTrans[14]))
+				- (BmatrixTrans[6] * (BmatrixTrans[9] * BmatrixTrans[15] - BmatrixTrans[11] * BmatrixTrans[13]))
+				+ (BmatrixTrans[7] * (BmatrixTrans[9] * BmatrixTrans[14] - BmatrixTrans[10] * BmatrixTrans[13]))))
+			- (BmatrixTrans[4] * (BmatrixTrans[4] * (BmatrixTrans[10] * BmatrixTrans[15] - BmatrixTrans[11] * BmatrixTrans[13]))
+				- (BmatrixTrans[5] * (BmatrixTrans[8] * BmatrixTrans[15] - BmatrixTrans[11] * BmatrixTrans[12]))
+				+ (BmatrixTrans[7] * (BmatrixTrans[8] * BmatrixTrans[13] - BmatrixTrans[9] * BmatrixTrans[12])))
+			+ (BmatrixTrans[2] * (BmatrixTrans[4] * (BmatrixTrans[9] * BmatrixTrans[15] - BmatrixTrans[11] * BmatrixTrans[13]))
+				- (BmatrixTrans[5] * (BmatrixTrans[8] * BmatrixTrans[15] - BmatrixTrans[11] * BmatrixTrans[12]))
+				+ (BmatrixTrans[7] * (BmatrixTrans[8] * BmatrixTrans[13] - BmatrixTrans[9] * BmatrixTrans[12])))
+			- (BmatrixTrans[3] * (BmatrixTrans[4] * (BmatrixTrans[9] * BmatrixTrans[14] - BmatrixTrans[10] * BmatrixTrans[13]))
+				- (BmatrixTrans[5] * (BmatrixTrans[8] * BmatrixTrans[14] - BmatrixTrans[10] * BmatrixTrans[12]))
+				+ (BmatrixTrans[6] * (BmatrixTrans[8] * BmatrixTrans[13] - BmatrixTrans[9] * BmatrixTrans[12])));
+
+		std::cout << "행렬식의 값" << std::endl;
+		std::cout << "첫 번째 행렬 : " << AmatrixDet << "\n" << "두 번째 행렬 : " << BmatrixDet << std::endl;
+
+		return 0;
+	}
+	else if (CommandOpt == 115) {
+		LoadingMakingMatrixs();
+		ShowMatrixs();
+		return 0;
+	}
+	else if (CommandOpt >= 49 && CommandOpt <= 57) { // 입력한 숫자를 행렬에 곱
+		std::cout << "행렬에 " << CommandOpt << "을(를) 곱" << std::endl;
+
+		int AmatrixNum[16], BmatrixNum[16];
+		for (auto i = 0; i < 16; ++i) {
+			AmatrixNum[i] = Amatrix[i] * (CommandOpt - 48);
+			BmatrixNum[i] = Bmatrix[i] * (CommandOpt - 48);
+		}
+		int i = 0;
+
+		while (i < 13) {
+			std::cout << "|" << AmatrixNum[i] << " " << AmatrixNum[i + 1] << " " << AmatrixNum[i + 2] << " " << AmatrixNum[i + 3]
+				<< "||" << BmatrixNum[i] << " " << BmatrixNum[i + 1] << " " << BmatrixNum[i + 2] << " " << BmatrixNum[i + 3] << "|" << std::endl;
+			i += 4;
+		}
+
+		std::cout << std::endl;
+		return 0;
+	}
+	else if (CommandOpt == 113) { // 프로그램 종료
+		std::cout << "프로그램을 종료합니다." << std::endl;
+		exit(0);
 	}
 
-	for (int i = 0; i < 13; i += 4) {
+	for (auto i = 0; i < 13; i += 4) {
 		std::cout << "|" << NewMatrix[i] << " " << NewMatrix[i + 1] << " " << NewMatrix[i + 2] << " " << NewMatrix[i + 3]
 			<< "|" << std::endl;
 	}
