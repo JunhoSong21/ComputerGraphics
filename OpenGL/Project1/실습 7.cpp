2a23b57ffe4c3d83eb32bd5708426e8d69beee19
JunhoSong21 <junhoasd1zxc2@gmail.com>
2024-10-07 20:05:07 +0900

    Summary 7 Start

---
+++ OpenGL/Project1/실습 7.cpp
@@ -0,0 +1,163 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+void InitBuffer();
+void make_shaderProgram();
+void make_vertexShaders();
+void make_fragmentShaders();
+
+char* filetobuf(const char* file) {
+	FILE* fptr;
+	long length;
+	char* buf;
+
+	fptr = fopen(file, "rb");
+	if (!fptr) return nullptr;
+	fseek(fptr, 0, SEEK_END);
+	length = ftell(fptr);
+	buf = (char*)malloc(length + 1);
+	fseek(fptr, 0, SEEK_SET);
+	fread(buf, length, 1, fptr);
+	fclose(fptr);
+	buf[length] = 0;
+
+	return buf;
+}
+
+const GLfloat triShape[3][3] = {
+	{-0.5, -0.5, 0.0}, {0.5, -0.5, 0.0}, {0.0, 0.5, 0.5}
+};
+
+const GLfloat colors[3][3] = {
+	{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}
+};
+
+GLuint vao, vbo[2];
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+GLuint vertexShader, fragmentShader; //--- 세이더 객체
+GLuint shaderProgramID; //--- 셰이더 프로그램
+
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Example 7");
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	make_shaderProgram();
+	InitBuffer();
+
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+GLvoid drawScene() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shaderProgramID);
+
+	glBindVertexArray(vao);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+}
+
+void InitBuffer() {
+	glGenVertexArrays(1, &vao);
+
+	glBindVertexArray(vao);
+
+	glGenBuffers(2, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//--- attribute 인덱스 0번을 사용가능하게 함
+	glEnableVertexAttribArray(0);
+	//--- 2번째 VBO를 활성화 하여 바인드 하고, 버텍스 속성 (색상)을 저장
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	//--- 변수 colors에서 버텍스 색상을 복사한다.
+	//--- colors 배열의 사이즈: 9 *float 
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
+	//--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	//--- attribute 인덱스 1번을 사용 가능하게 함.
+	glEnableVertexAttribArray(1);
+}
+
+void make_shaderProgram() {
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	//-- shader Program
+	shaderProgramID = glCreateProgram();
+	glAttachShader(shaderProgramID, vertexShader);
+	glAttachShader(shaderProgramID, fragmentShader);
+	glLinkProgram(shaderProgramID);
+	//--- 세이더 삭제하기
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//--- Shader Program 사용하기
+	glUseProgram(shaderProgramID);
+}
+
+
+void make_vertexShaders()
+{
+	vertexSource = filetobuf("vertex7.glsl");
+	//--- 버텍스 세이더 객체 만들기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+	//--- 버텍스 세이더 컴파일하기
+	glCompileShader(vertexShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+
+	if(!result) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	fragmentSource = filetobuf("fragment7.glsl");
+	//--- 프래그먼트 세이더 객체 만들기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//--- 세이더 코드를 세이더 객체에 넣기
+	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+	//--- 프래그먼트 세이더 컴파일
+	glCompileShader(fragmentShader);
+	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+
+	if (!result) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
